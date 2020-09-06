1b144a2045894afdb91fe7094e461c94e601e4d6
MAC <MAC@MACs-iMac.local>
2020-09-06 05:51:47 +0530

    Initial files

---
+++ main.py
@@ -0,0 +1,56 @@
+# from __future__ import print_function
+# for i in range(1, 5):
+#     for j in range(1, 10):
+#         print(j, end='')
+#         if i > 1:
+#             print('.', end='')
+#             for k in range(1, i):
+#                 print(j, end='')
+#         print(' ', end='')
+#     print('\n')
+
+# a = [1, 2, 3, 4]
+# b= a[::-1]
+#
+# b[0] = 100
+#
+# print(a)
+# print(b)
+
+
+# a, b, c = (1, 2, 'Reshma')
+# print(c.split())
+
+# a = ('a', 'z', 'k')
+#
+# del a[2]
+
+# person = {
+#     "name": "Ganesh",
+#     "age": 33
+# }
+#
+# print(person.items())
+
+# addNumbers = lambda num1, num2: num1 + num2
+#
+# print(addNumbers(1, 2))
+
+# a= {
+#     "name": "Ganesh"
+# }
+#
+# b = a.copy()
+# print(b is a)
+
+# from datetime import date
+#
+# print(date.today())
+
+# import time
+#
+# print(time.localtime())
+
+
+
+
